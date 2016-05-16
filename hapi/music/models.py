091e44d3443622e92c13c9c8d86ac76635c8e562
Marcos Hernández <marcoslhc@gmail.com>
2016-05-15 21:19:14 -0400

    Fix migration problem, extend the auth user model

--- hapi/music/models.py
+++ hapi/music/models.py
@@ -12,6 +12,3 @@ class Album(models.Model):
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        app_label = 'MusicAlbum'
--- hapi/settings.py
+++ hapi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'hapi.users',
+    'hapi.music'
 ]
 
 REST_FRAMEWORK = {
